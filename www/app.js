d30eb5bba3fc496d1150a8aa188fc4672771a38d
AKSpikey <arunarularasi24@gmail.com>
2015-12-10 00:04:25 +0530

    Database connection

--- www/app.js
+++ www/app.js
@@ -1,5 +1,18 @@
 var app = angular.module("homeApp", ['ngRoute']);
 
+app.controller("databaseController", function($scope){
+	var myFirebaseRef = new Firebase("https://brilliant-inferno-7097.firebaseio.com/");
+	$scope.register = function(){
+		myFirebaseRef.push({
+			name:$scope.name,
+			blood:$scope.blood,
+			phone:$scope.phone,
+			location:$scope.location
+		});
+		$scope.registerForm = {};
+	}
+})
+
 app.config(function($routeProvider){
 	$routeProvider.when('/home',{
 		templateUrl: "pages/home.html"
