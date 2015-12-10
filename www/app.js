290388441527f56cebb55b24521b0e090ceb3731
AKSpikey <arunarularasi24@gmail.com>
2015-12-11 00:54:11 +0530

    Search Details Push

--- www/app.js
+++ www/app.js
@@ -1,4 +1,27 @@
-var app = angular.module("homeApp", ['ngRoute']);
+var app = angular.module("homeApp", ['ngRoute','firebase']);
+
+app.service('productService', function() {
+  var searchList = [];
+
+  var addSearch = function(newObj) {
+      searchList.push(newObj);
+  };
+
+  var getSearch = function(){
+      return searchList;
+  };
+
+  return {
+    addSearch: addSearch,
+    getSearch: getSearch
+  };
+
+});
+
+// app.factory("getData", ['$firebase', function($firebase){
+// 	var data = new Firebase("https://brilliant-inferno-7097.firebaseio.com/");
+// 	return $firebase(data).$asArray();
+// }]);
 
 app.controller("databaseController", function($scope){
 	var myFirebaseRef = new Firebase("https://brilliant-inferno-7097.firebaseio.com/");
@@ -11,14 +34,32 @@ app.controller("databaseController", function($scope){
 		});
 		$scope.registerForm = {};
 	}
-})
+});
+
+app.controller("searchController", function($scope,$window,productService){
+	$scope.search = function(){
+		var currObj = {"blood":$scope.bloodtype, "locations":$scope.locations};
+        productService.addSearch(currObj);
+    	$window.location.href = "#/searchresult";
+	}
+});
+
+app.controller("searchresultController", function($scope,$window,productService){
+	// $scope.datas = getData;
+	$scope.result = productService.getSearch();
+});
 
 app.config(function($routeProvider){
 	$routeProvider.when('/home',{
 		templateUrl: "pages/home.html"
 	})
 	.when('/search',{
-		templateUrl: "pages/search.html"
+		templateUrl: "pages/search.html",
+		controller: 'searchController'
+	})
+	.when('/searchresult',{
+		templateUrl: "pages/searchresult.html",
+		controller: 'searchresultController'
 	})
 	.when('/register',{
 		templateUrl: "pages/register.html"
